277681c308a03e463c63f74bfb883ad3a168051b
배현중 <alex950107@gmail.com>
2022-07-25 15:36:29 +0900

    storesettings

---
+++ frontend/src/store/getters.js
@@ -0,0 +1,8 @@
+export default {
+  fetchedItem(state) {
+    return state.item;
+  },
+  fetchedUser(state) {
+    return state.user;
+  },
+};
--- frontend/src/views/MypageView.vue
+++ frontend/src/views/MypageView.vue
@@ -1,4 +1,6 @@
-<template></template>
+<template>
+  <div>mypage</div>
+</template>
 
 <script>
 export default {};

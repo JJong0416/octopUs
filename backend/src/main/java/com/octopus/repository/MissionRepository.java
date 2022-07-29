68811dc150a1c831aef40e830052cfe194e0e6cf
rudwns06 <rudwns06@hanmail.net>
2022-07-29 16:47:18 +0900

    fix : mission의 return값 변화

--- backend/src/main/java/com/octopus/repository/MissionRepository.java
+++ backend/src/main/java/com/octopus/repository/MissionRepository.java
@@ -1,11 +1,16 @@
 package com.octopus.repository;
 
 import com.octopus.domain.Mission;
+import com.octopus.domain.dto.MissionListDto;
+import com.octopus.domain.type.MissionOpenType;
+import com.octopus.domain.type.MissionStatus;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface MissionRepository extends JpaRepository<Mission, Long> {
-    List<Mission> findTop5By(Sort sort);
+    List<Mission> findTop5ByMissionStatusAndMissionOpen(Sort sort, MissionStatus missionStatus, MissionOpenType missionOpenType);
+
+    List<MissionListDto> findTop5By(Sort sort);
 }
--- backend/src/main/java/com/octopus/service/MissionService.java
+++ backend/src/main/java/com/octopus/service/MissionService.java
@@ -1,12 +1,15 @@
 package com.octopus.service;
 
 import com.octopus.domain.Mission;
+import com.octopus.domain.dto.MissionListDto;
 import com.octopus.repository.MissionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.octopus.domain.type.MissionOpenType;
+import com.octopus.domain.type.MissionStatus;
 import java.util.List;
 
 @Service
@@ -16,7 +19,14 @@ public class MissionService {
     private final MissionRepository missionRepository;
 
     @Transactional(readOnly = true)
-    public List<Mission> getNewMissions(){
-        return missionRepository.findTop5By(Sort.by(Sort.Direction.DESC,"missionNo"));
+    public List<MissionListDto> getNewMissions(){
+//        return missionRepository.findTop5ByMissionStatusAndMissionOpen(
+//                Sort.by(Sort.Direction.DESC,"missionNo"),
+//                MissionStatus.OPEN,
+//                MissionOpenType.OPEN_ROOM
+//        );
+        return missionRepository.findTop5By(
+                Sort.by(Sort.Direction.DESC,"missionNo")
+        );
     }
 }

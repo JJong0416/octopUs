e84894f5fc3398176107c4d12f6b9b6d12c028d6
배현중 <alex950107@gmail.com>
2022-08-02 14:10:47 +0900

    CameraApi

--- frontend/src/views/ProceedingMission.vue
+++ frontend/src/views/ProceedingMission.vue
@@ -165,12 +165,29 @@
         </div>
       </v-expand-transition>
     </v-card>
+    <div>
+      <video ref="video" id="video" width="500" height="500" autoplay></video>
+      <div>
+        <button color="info" id="snap" v-on:click="capture()">
+          Snap Photo
+        </button>
+      </div>
+      <canvas ref="canvas" id="canvas" width="500" height="500"></canvas>
+      <ul>
+        <li class="capture" v-for="c in captures" v-bind:key="c.d">
+          <img v-bind:src="c" height="50" />
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   data: () => ({
+    video: {},
+    canvas: {},
+    captures: [],
     show: false,
     calendarShow: false,
     focus: "",
@@ -207,8 +224,21 @@ export default {
   }),
   mounted() {
     this.$refs.calendar.checkChange();
+    this.video = this.$refs.video;
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+        this.video.srcObject = stream;
+        this.video.play();
+      });
+    }
   },
   methods: {
+    capture() {
+      this.canvas = this.$refs.canvas;
+      this.canvas.getContext("2d").drawImage(this.video, 0, 0, 500, 500);
+      this.captures.push(this.canvas.toDataURL("image/png"));
+      console.log(this.captures);
+    },
     viewDay({ date }) {
       this.focus = date;
       this.type = "day";

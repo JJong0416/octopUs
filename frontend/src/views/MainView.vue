7acedf57b8cd21b4cec544356d15e51df01dbaca
soomi1015 <99255195+soomi1015@users.noreply.github.com>
2022-08-03 22:32:14 +0900

    Update Main

--- frontend/src/views/MainView.vue
+++ frontend/src/views/MainView.vue
@@ -51,22 +51,56 @@
         >
         <v-col cols="2"></v-col>
       </v-row>
+      <!-- Hot 실시간 반영 -->
+      <v-row>
+        <v-col cols="2"><v-img src="../assets/img/Hot.png"></v-img></v-col>
+        <v-col cols="7">
       <v-carousel
         cycle
         interval="1500"
-        height="400"
+        height="80"
         hide-delimiter-background
         hide-delimiters
         show-arrows-on-hover
       >
         <v-carousel-item v-for="(slide, i) in slides" :key="i">
-          <v-sheet :color="colors[i]" height="100%">
+          <v-sheet height="100%">
             <v-row class="fill-height" align="center" justify="center">
-              <div class="text-h2">{{ slide }} Slide</div>
+              <div class="text-h4">{{ slide }}</div>
             </v-row>
           </v-sheet>
         </v-carousel-item>
       </v-carousel>
+</v-col>
+
+      <!-- Hot 더보기 -->
+      <v-col cols="3">
+      <v-btn  color="red lighten-1">more</v-btn></v-col>
+</v-row>
+      <!-- New 실시간 반영 -->
+      <v-row>
+        <v-col cols="2"><v-img src="../assets/img/New.png"></v-img></v-col>
+        <v-col cols="7">
+      <v-carousel
+        cycle
+        interval="1500"
+        height="80"
+        hide-delimiter-background
+        hide-delimiters
+        show-arrows-on-hover
+      >
+        <v-carousel-item v-for="(slide, i) in slides" :key="i">
+          <v-sheet  height="100%">
+            <v-row class="fill-height" align="center" justify="center">
+              <div class="text-h4">{{ slide }}</div>
+            </v-row>
+          </v-sheet>
+        </v-carousel-item>
+      </v-carousel></v-col>
+      <!-- New 더보기 -->
+      <v-col cols="3">
+      <v-btn color="warning">more</v-btn></v-col>
+      </v-row>
     </v-container>
   </div>
 </template>
@@ -75,13 +109,6 @@
 export default {
   data: () => ({
     items: ["코드 입력", "제목 검색", "테마 검색"],
-    colors: [
-      "indigo",
-      "warning",
-      "pink darken-2",
-      "red lighten-1",
-      "deep-purple accent-4",
-    ],
     slides: ["First", "Second", "Third", "Fourth", "Fifth"],
   }),
 };

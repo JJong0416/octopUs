d56d63947e833eb8493e289b78bc28b761bcc98b
rudwns06 <rudwns06@hanmail.net>
2022-08-08 16:32:06 +0900

    feature : 마이페이지에서 참여중인방, 종료된방 구현

--- frontend/src/views/MypageView.vue
+++ frontend/src/views/MypageView.vue
@@ -1,58 +1,210 @@
 <template>
-  <div>
-    <v-img height="30vh" src="../assets/img/1.png">
-      <!-- 이미지 수정 버튼 -->
+  <v-container>
+    <v-card style="background-color: #ffdede">
+      <v-img height="250" src="../assets/img/1.png">
+        <!-- 이미지 수정 버튼 -->
+        <v-row>
+          <v-col cols="8"></v-col>
+          <v-col cols="2">
+            <v-btn small class="ma-2" outlined fab color="indigo">
+              <v-icon>mdi-pencil</v-icon>
+            </v-btn></v-col
+          >
+        </v-row>
+      </v-img>
+      <!-- Nickname -->
       <v-row>
-        <v-col cols="10"></v-col>
-        <v-col cols="1">
-          <v-btn small class="ma-0" outlined fab color="indigo">
+        <v-col cols="10">
+          <v-card-title>{{ userInfo.userNickname }}</v-card-title></v-col
+        >
+        <v-col cols="2">
+          <v-btn small class="ma-2">
             <v-icon>mdi-pencil</v-icon>
           </v-btn></v-col
         >
-        <v-col cols="1"></v-col>
       </v-row>
-    </v-img>
-    <!-- Nickname -->
-    <v-row>
-      <v-col cols="10">
-        <v-card-title>{{ userInfo.userNickname }}</v-card-title></v-col
-      >
-      <v-col cols="2">
-        <v-btn small class="ma-2" outlined fab color="indigo">
-          <v-icon>mdi-pencil</v-icon>
-        </v-btn></v-col
-      >
-    </v-row>
-    <!-- id, Email -->
-    <v-card-text>
-      <div>Id : {{ userInfo.userId }}</div>
-      <div>Email : {{ userInfo.userEmail }}</div>
-    </v-card-text>
+      <!-- id, Email -->
+      <v-card-text>
+        <div>Id : {{ userInfo.userId }}</div>
+        <div>Email : {{ userInfo.userEmail }}</div>
+      </v-card-text>
 
-    <v-divider class="mx-4"></v-divider>
+      <v-divider class="mx-4"></v-divider>
 
-    <v-row>
-      <v-col cols="6">
-        <v-card-title>{{ userInfo.userPoint }} Point</v-card-title>
-      </v-col>
-      <v-col cols="3"> 충전하기 </v-col>
-      <v-col cols="3">
-        <v-img style="max-width: 70px" src="../assets/kakaopay.png"></v-img>
-      </v-col>
-    </v-row>
+      <v-row>
+        <v-col cols="6">
+          <v-card-title>{{ userInfo.userPoint }} Point</v-card-title>
+        </v-col>
+        <v-col cols="3"> 충전하기 </v-col>
+        <v-col cols="3">
+          <v-img style="max-width: 70px" src="../assets/kakaopay.png"></v-img>
+        </v-col>
+      </v-row>
 
-    <br />
-    비밀번호 수정하기
-    <v-btn small class="ma-2" outlined fab color="indigo">
-      <v-icon>mdi-pencil</v-icon>
-    </v-btn>
-    <v-divider class="mx-4"></v-divider>
-    <br />
-    <router-link :to="{ name: 'proceeding', params: { missionNo: 1 } }"
-      >참여 중인 방</router-link
-    >
-    <br />
-  </div>
+      <br />
+      비밀번호 수정하기
+      <v-btn small class="ma-2">
+        <v-icon>mdi-pencil</v-icon>
+      </v-btn>
+      <v-divider class="mx-4"></v-divider>
+      <br />
+        <v-data-table
+          :headers="Missionheaders"
+          :items="openedMissions" 
+          :items-per-page="5"
+          class="elevation-1"
+          :single-expand="singleExpand"
+          :expanded.sync="openexpanded" 
+          item-key="missionCode"
+          show-expand
+          @item-expanded="clickExpand"
+        >
+          <!-- 표 상단의 해더 문구 -->
+          <template v-slot:top>
+            <v-toolbar flat>
+              <v-toolbar-title>참여중인 방</v-toolbar-title>
+              <v-spacer></v-spacer>
+              <!-- <v-switch
+                v-model="singleExpand"
+                label="하나만 펼치기"
+                class="mt-2"
+              ></v-switch> -->
+            </v-toolbar>
+          </template>
+
+          <template v-slot:[`item.missionPoint`]="{ item }">
+            <v-chip
+              :color="getColor(item.missionPoint)"
+              dark
+            >
+              {{ item.missionPoint }}
+            </v-chip>
+          </template>
+          <!-- <template v-slot:[`item.data-table-expand`]="{ item }">
+            <tr @click="clickExpand(item.missionNo)">
+              <td>{{item.data-table-expand}}</td>
+            </tr>
+            나오냐?
+          </template> -->
+
+          <template v-slot:expanded-item="{ headers, item }">
+            <td :colspan="headers.length">
+
+<!-- 내부 내용 -->
+              <template>
+                  <v-list-item>
+                    <v-list-item-content>
+                      <v-list-item-title>미션 내용</v-list-item-title>
+                      <v-list-item-subtitle>{{item.missionContent}}</v-list-item-subtitle>
+                    </v-list-item-content>
+                  </v-list-item>
+
+                  <v-list-item two-line>
+                    <v-list-item-content>
+                      <v-list-item-title>입장 코드</v-list-item-title>
+                      <v-list-item-subtitle>{{item.missionCode}}</v-list-item-subtitle>
+                    </v-list-item-content>
+                  </v-list-item>
+
+                  <v-list-item two-line>
+                    <v-list-item-content>
+                      <v-list-item-title>분야</v-list-item-title>
+                      <v-list-item-subtitle>  {{item.missionType}}</v-list-item-subtitle>
+                    </v-list-item-content>
+                  </v-list-item>
+
+                  <v-list-item two-line>
+                    <v-list-item-content>
+                      <v-list-item-title>인증 사진</v-list-item-title>
+                      <img :src=URL style="height:120px" class="mx-4" />
+                    </v-list-item-content>
+                  </v-list-item>
+              </template>
+
+<!-- 내부 내용 끝 -->
+              <router-link
+                :to="{ name: 'proceeding', params: { missionNo: item.missionNo } }"
+              >
+                자세히보기
+              </router-link>
+            </td>
+          </template>
+        </v-data-table>
+
+
+      <br />
+        <v-data-table
+          :headers="Missionheaders"
+          :items="closedMissions"
+          :items-per-page="5"
+          class="elevation-1"
+          :single-expand="singleExpand"
+          :expanded.sync="closeexpanded" 
+          item-key="missionCode"
+          show-expand
+        >
+          <!-- 표 상단의 해더 문구 -->
+          <template v-slot:top>
+            <v-toolbar flat>
+              <v-toolbar-title>종료된 방</v-toolbar-title>
+              <v-spacer></v-spacer>
+            </v-toolbar>
+          </template>
+
+          <template v-slot:[`item.missionPoint`]="{ item }">
+            <v-chip
+              :color="getColor(item.missionPoint)"
+              dark
+            >
+              {{ item.missionPoint }}
+            </v-chip>
+          </template>
+          <template v-slot:expanded-item="{ headers, item }">
+            <td :colspan="headers.length">
+
+<!--  -->
+              <template>
+                  <v-list-item>
+                    <v-list-item-content>
+                      <v-list-item-title>미션 내용</v-list-item-title>
+                      <v-list-item-subtitle>{{item.missionContent}}</v-list-item-subtitle>
+                    </v-list-item-content>
+                  </v-list-item>
+
+                  <v-list-item two-line>
+                    <v-list-item-content>
+                      <v-list-item-title>입장 코드</v-list-item-title>
+                      <v-list-item-subtitle>{{item.missionCode}}</v-list-item-subtitle>
+                    </v-list-item-content>
+                  </v-list-item>
+
+                  <v-list-item two-line>
+                    <v-list-item-content>
+                      <v-list-item-title>분야</v-list-item-title>
+                      <v-list-item-subtitle>  {{item.missionType}}</v-list-item-subtitle>
+                    </v-list-item-content>
+                  </v-list-item>
+
+                  <v-list-item two-line>
+                    <v-list-item-content>
+                      <v-list-item-title>인증 사진</v-list-item-title>
+                      <v-list-item-subtitle>{{item.missionCode}}</v-list-item-subtitle>
+                    </v-list-item-content>
+                  </v-list-item>
+              </template>
+
+<!--  -->
+              <router-link
+                :to="{ name: 'after', params: { missionNo: item.missionNo } }"
+              >
+                자세히보기
+              </router-link>
+            </td>
+          </template>
+        </v-data-table>
+      <br/>
+    </v-card>
+  </v-container>
 </template>
 <script>
 import axios from "axios";
@@ -62,8 +214,54 @@ export default {
   data() {
     return {
       userInfo: [],
+      openedMissions: [],
+      closedMissions: [],
+      Missionheaders:[
+        {
+          text: `방 이름`,
+          align: `start`,
+          softable: false,
+          value: `missionTitle`,
+        },
+        {text:`포인트`,value:`missionPoint`},
+        {text: ``, value: `data-table-expand` },
+      ],
+      openexpanded: [],
+      closeexpanded: [],
+      singleExpand: true,
+      URL:"",
     };
   },
+
+  methods: {
+      getColor (point) {
+        if (point > 400) return 'red'
+        else if (point > 200) return 'orange'
+        else return 'green'
+      },
+
+      clickExpand({item}) {
+        var vm=this;
+        console.log(item);
+        axios.get(`api/mission/${item.missionNo}/picture`,{
+          headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json",
+          },
+        })
+        .then(function(response){
+          console.log(response);
+          // 내부에 받은 이미지 확인필요
+          console.log(response.data[0].userPictures[0].pictureUrl);
+
+          vm.URL = response.data[0].userPictures[0].pictureUrl;
+          console.log(vm.URL)
+        })
+        .catch(function(err){
+          console.log(err);
+        });
+      },
+    },
   created() {
     var vm = this;
     let token = cookie.get("token");
@@ -79,10 +277,35 @@ export default {
       .then(function (response) {
         console.log(response);
         vm.userInfo = response.data;
+        // 참여중인 방 정보 받기
+        axios
+          .get(`api/user/mission/${vm.userInfo.userId}`, {
+            headers: {
+              "Access-Control-Allow-Origin": "*",
+              "Content-Type": "application/json",
+            },
+          })
+          .then(function (response) {
+            console.log(response);
+            response.data.forEach(mission => {
+              if(mission.missionStatus==="CLOSE"){
+                vm.closedMissions.push(mission)
+              }else{
+                vm.openedMissions.push(mission)
+              }
+            });
+            console.log("열린것" + vm.openedMissions);
+            console.log("닫힌것" + vm.closedMissions);
+          })
+          .catch(function (err) {
+            console.log(err);
+          });
       })
       .catch(function (err) {
         console.log(err);
       });
+
+
   },
 };
 </script>

69b3fa9fcaf3e0999e26cf7bfa4ea914fbcb73f1
rudwns06 <rudwns06@hanmail.net>
2022-08-11 22:51:53 +0900

    fix: 메인의 아바타를 양식에 맞게 받는거로 변경

--- frontend/src/views/MainView.vue
+++ frontend/src/views/MainView.vue
@@ -4,14 +4,27 @@
     <v-container>
       <v-row class="animate__animated animate__bounce">
         <v-col class="logo-img-wrapper">
-          <v-img :src="require(`../assets/img/Ocsoon/Pet/${userAvatar[3]}.png`)">
-          <v-img :src="require(`../assets/img/Ocsoon/Character/${userAvatar[0]}.png`)">
-          <v-img :src="require(`../assets/img/Ocsoon/Face/${userAvatar[1]}.png`)">
-          <v-img :src="require(`../assets/img/Ocsoon/Hat/${userAvatar[2]}.png`)"></v-img></v-img>
-          </v-img></v-img>
+          <v-img
+            :src="require(`../assets/img/Ocsoon/Pet/${userAvatar[3]}.png`)"
+          >
+            <v-img
+              :src="
+                require(`../assets/img/Ocsoon/Character/${userAvatar[0]}.png`)
+              "
+            >
+              <v-img
+                :src="require(`../assets/img/Ocsoon/Face/${userAvatar[1]}.png`)"
+              >
+                <v-img
+                  :src="
+                    require(`../assets/img/Ocsoon/Hat/${userAvatar[2]}.png`)
+                  "
+                ></v-img
+              ></v-img> </v-img
+          ></v-img>
         </v-col>
       </v-row>
-      
+
       <!-- 검색창 -->
       <v-row>
         <v-col class="py-0" cols="4">
@@ -47,7 +60,8 @@
             height="80"
             hide-delimiter-background
             hide-delimiters
-            show-arrows-on-hover>
+            show-arrows-on-hover
+          >
             <v-carousel-item v-for="(hotmission, i) in hotmissions" :key="i">
               <v-sheet height="100%">
                 <v-row class="fill-height" align="center" justify="center">
@@ -65,7 +79,8 @@
             height="80"
             hide-delimiter-background
             hide-delimiters
-            show-arrows-on-hover>
+            show-arrows-on-hover
+          >
             <v-carousel-item v-for="(newmission, i) in newmissions" :key="i">
               <v-sheet height="100%">
                 <v-row class="fill-height" align="center" justify="center">
@@ -77,9 +92,8 @@
         </v-col>
       </v-row>
     </v-container>
-      <footer-view></footer-view>
+    <footer-view></footer-view>
   </div>
-
 </template>
 
 <script>
@@ -87,11 +101,11 @@ import { mapState } from "vuex";
 import axios from "axios";
 import HeaderView from "../components/common/HeaderView.vue";
 import FooterView from "../components/common/FooterView.vue";
-import 'animate.css';
+import "animate.css";
 const memberStore = "memberStore";
 
 export default {
-  components:{
+  components: {
     HeaderView,
     FooterView,
   },
@@ -100,19 +114,19 @@ export default {
     newmissions: [],
     items: ["코드 입력", "제목 검색", "테마 검색"],
     userInfo2: [],
-    userAvatar:[0,0,0,0],
+    userAvatar: [0, 0, 0, 0],
   }),
   computed: {
     ...mapState(memberStore, ["isLogin", "userInfo"]),
   },
 
-  methods:{
-    moveHot(){
-      this.$router.push('/hotnew')
+  methods: {
+    moveHot() {
+      this.$router.push("/hotnew");
     },
-    moveNew(){
-      this.$router.push('/hotnew')
-    }
+    moveNew() {
+      this.$router.push("/hotnew");
+    },
   },
   created() {
     var vm = this;
@@ -126,14 +140,14 @@ export default {
       })
       .then(function (response) {
         vm.newmissions = response.data;
-        console.log("들어온 newmissions : ")
+        console.log("들어온 newmissions : ");
         console.log(vm.newmissions);
       })
       .catch(function (err) {
         console.log(err);
       });
     // hot mission
-      axios
+    axios
       .get(`api/mission/hot`, {
         headers: {
           "Access-Control-Allow-Origin": "*",
@@ -142,14 +156,14 @@ export default {
       })
       .then(function (response) {
         vm.hotmissions = response.data;
-        console.log("들어온 hotmissions : ")
+        console.log("들어온 hotmissions : ");
         console.log(vm.hotmissions);
       })
       .catch(function (err) {
         console.log(err);
       });
-      
-      axios
+
+    axios
       .get(`api/user/info`, {
         headers: {
           "Access-Control-Allow-Origin": "*",
@@ -159,9 +173,9 @@ export default {
       .then(function (response) {
         console.log(response);
         vm.userInfo2 = response.data;
-        vm.userAvatar=vm.userInfo2.userAvatar.split(',');
+        vm.userAvatar = vm.userInfo2.userAvatar.split(", ");
         console.log(vm.userAvatar);
-        })
+      })
       .catch(function (err) {
         console.log(err);
       });
@@ -170,7 +184,6 @@ export default {
 </script>
 
 <style>
-
 .signup a {
   text-decoration: none;
   color: black;
@@ -196,7 +209,7 @@ body {
   align-items: center;
   padding: 0;
 }
-.app{
+.app {
   background-color: #eefaec;
 }
 </style>

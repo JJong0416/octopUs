fa20dccfd67bcf0ed43d3b84cb31fc06cf5c12c4
rudwns06 <rudwns06@hanmail.net>
2022-08-11 13:01:16 +0900

    fix : 방만들기시 유효성검사 + 백으로 정보넘기기 성공

--- frontend/src/views/CreateView.vue
+++ frontend/src/views/CreateView.vue
@@ -53,7 +53,7 @@
           v-model="mission.missionContent"
           :rules="descriprules"
           counter
-          maxlength="200"
+          maxlength="300"
           hint="미션에 대한 설명, 달성조건, 사람을 모을 멘트까지!"
           label="미션에 대해 소개해주세요!"
         ></v-text-field>
@@ -93,6 +93,7 @@ export default {
       missionContent: null,
       missionOpen: true,
     },
+    userInfo: [],
     pointrules: [
       (value) => !!value || "참여 포인트를 입력해주세요",
       (value) => (value && value.length >= 3) || "최소 100이상이어야 합니다.",
@@ -106,13 +107,36 @@ export default {
 
     rules: [
       (value) => !!value || "미션의 제목을 입력해주세요",
-      (value) => (value || "").length <= 20 || "20자까지 입력가능합니다.",
+      (value) =>
+        (value && (value || "").length >= 4 && (value || "").length <= 30) ||
+        "4 ~ 30자 사이여야합니다.",
     ],
     slider: 8,
     title: "Preliminary report",
     description: "이방은 어쩌구 저쩌구 솰라솰라",
-    descriprules: [(v) => v.length <= 200 || "200자까지 입력가능합니다."],
+    descriprules: [
+      (v) =>
+        (v && v.length <= 300 && v.length >= 6) ||
+        "6~300자까지 입력가능합니다.",
+    ],
   }),
+  created() {
+    var vm = this;
+    axios
+      .get(`api/user/info`, {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json",
+        },
+      })
+      .then(function (response) {
+        console.log(response);
+        vm.userInfo = response.data;
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  },
   computed: {
     dateRangeText() {
       return this.dates.join(" ~ ");
@@ -143,10 +167,11 @@ export default {
           missionPoint: this.mission.missionPoint,
           missionLimitPersonnel: this.mission.missionLimitPersonnel,
           missionContent: this.mission.missionContent,
-          missionOpen: this.mission.missionOpen,
+          missionOpen: this.mission.missionOpen ? 0 : 1,
         })
         .then(() => {
-          alert("미션생성완료");
+          alert("미션 성공여부를 위한 인증시간을 정하면 방이 활성화되요!");
+          // this.$router.push({name="/before" ,params:missionNo});
         })
         .catch(() => {
           alert("미션생성실패");

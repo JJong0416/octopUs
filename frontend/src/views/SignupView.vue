d92814dc05cd41778131f84c6dc02cdf2eac77a4
rudwns06 <rudwns06@hanmail.net>
2022-08-13 21:38:29 +0900

    fix: 회원가입시 해당 도메인,이메일로 회원가입되었는가 확인 로직 추가

--- frontend/src/views/SignupView.vue
+++ frontend/src/views/SignupView.vue
@@ -1,159 +1,169 @@
 <template>
-<div>
-  <v-container>
-    <v-row class="py-3">
-      <v-col cols="4">
-        <!-- 뒤로 가기 버튼 -->
-        <v-btn @click="goback" icon>
-          <v-icon> mdi-arrow-left</v-icon>
-        </v-btn>
-      </v-col>
-      <v-col cols="3"></v-col>
-      <v-col cols="5">
-        <!-- 회원 가입 버튼 -->
-        <v-btn outlined @click="login" color="#143559">
-          로그인하러 가기
-        </v-btn>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-alert v-if="isError" type="error">
+  <div>
+    <v-container>
+      <v-row class="py-3">
+        <v-col cols="4">
+          <!-- 뒤로 가기 버튼 -->
+          <v-btn @click="goback" icon>
+            <v-icon> mdi-arrow-left</v-icon>
+          </v-btn>
+        </v-col>
+        <v-col cols="3"></v-col>
+        <v-col cols="5">
+          <!-- 회원 가입 버튼 -->
+          <v-btn outlined @click="login" color="#143559">
+            로그인하러 가기
+          </v-btn>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-alert v-if="isError" type="error">
           {{ errorMsg }}
         </v-alert>
-    </v-row>
-    <v-form ref="form" v-model="valid" lazy-validation>
-    <v-row class="pl-3">
-      <h4>이메일을 통해 본인임을 인증합니다.</h4>
-    </v-row>
-    <v-row>
-      <v-col class="py-0 pr-3" cols="9">
-        <v-text-field
-          v-model="user.email"
-          :rules="emailRules"
-          label="사용가능한 이메일을 입력해주세요."
-        ></v-text-field>
-      </v-col>
-      <v-col class="px-0" cols="3">
-        <v-btn class="px-0" @click="sendemail">인증하기</v-btn>
-      </v-col>
-    </v-row>
-    <v-row v-if="issendemail">
-      <v-col class="py-0 pr-3" cols="8">
-        <v-text-field
-          v-model="aouthcode"
-          required
-          label="인증번호 입력"
-        ></v-text-field>
-      </v-col>
-      <v-col class="px-0" cols="3">
-        <v-btn class="px-0" @click="codecheck">인증번호 확인</v-btn>
-      </v-col>
-    </v-row>
-    <!-- email 인증 추가하기 -->
-    <v-row>
-      <v-col class="py-0" cols="8">
-        <!-- id input -->
-        <v-text-field
-          v-model="user.userid"
-          label="아이디를 입력해주세요."
-          :rules="[rules.required]"
-          @change="userIdChk = false"
-        ></v-text-field>
-      </v-col>
-      <v-col cols="4">
-        <v-btn class="px-0"  @click="idcheck">중복 검사</v-btn>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col class="py-0">
-        <!-- password input -->
-        <v-text-field
-          v-model="user.userpwd"
-          :append-icon="show? 'mdi-eye' : 'mdi-eye-off'"
-          :rules="[rules.required, rules.min]"
-          :type="show? 'text' : 'password'"
-          label="비밀번호를 설정해주세요."
-          hint="8자 이상 입력하세요"
-          @click:append="show = !show"
-          counter
-        ></v-text-field>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col class="py-0">
-        <!-- password input again -->
-        <v-text-field
-          v-model="chkPassword"
-          :append-icon="show? 'mdi-eye' : 'mdi-eye-off'"
-          :rules="[rules.required, rules.min]"
-          :type="show? 'text' : 'password'"
-          label="비밀번호를 다시 한 번 입력해주세요."
-          hint="8자 이상 입력하세요"
-          @click:append="show = !show"
-          counter
-        ></v-text-field>
-      </v-col>
-    </v-row>
-    <v-row justify="center">
-      <h5 v-if="!sameChk(chkPassword)" class="pink--text">
-        비밀번호가 같지 않습니다. 다시 한 번 확인해주세요.
-      </h5>
-    </v-row>
-    <v-row>
-      <v-col class="py-0" cols="8">
-        <v-form ref="nicknameForm" lazy-validation>
-        <!-- nickname input -->
-        <v-text-field
-          v-model="user.usernickname"
-          :counter="10"
-          label="닉네임을 입력해주세요."
-          :rules="nameRules"
-          @change="userNickChk = false"
-        ></v-text-field></v-form>
-      </v-col>
-      <v-col cols="4">
-        <v-btn class="px-0" @click="nickcheck"  @keyup.enter="nickcheck">중복 검사</v-btn>
-      </v-col>
-    </v-row>
-    <v-row justify="center">
-      <h5>옥순이 캐릭터를 골라주세요. 차후 포인트 차감 후 변경 가능합니다.</h5>
-    </v-row>
-    <v-row justify="center">
-      <v-img max-width="30%" :src="require(`../assets/img/Ocsoon/Character/${this.user.avatar}.png`)">
-        <v-img src="../assets/img/Ocsoon/Face/0.png"></v-img>
-      </v-img>
-    </v-row>
-    <v-row justify="center" class="py-0">
-      <v-btn max-height="30" color="#fa183e" @click="changeColor(0)"/>
-      <v-btn max-height="30" color="#ffbbed" @click="changeColor(1)"/>
-      <v-btn max-height="30" color="#ffec00" @click="changeColor(2)"/>
-      <v-btn max-height="30" color="#b9ffb2" @click="changeColor(3)"/>
-    </v-row>
-    <v-row justify="center" class="py-0">
-      <v-btn max-height="30" color="#a5bbff" @click="changeColor(7)"/>
-      <v-btn max-height="30" color="#003fff" @click="changeColor(4)"/>
-      <v-btn max-height="30" color="#d2a1ff" @click="changeColor(5)"/>
-      <v-btn max-height="30" color="#8500ff" @click="changeColor(6)"/>
-    </v-row>
-    <v-row>
-      <v-col>
-        <v-btn outlined @click="reset" color="red">Reset</v-btn>
-      </v-col>
-      <v-col></v-col>
-      <v-col>
-        <v-btn color="#ffadad" @click="register()">회원가입하기</v-btn>
-      </v-col>
-    </v-row>
-    </v-form>
-  </v-container>
-</div>
+      </v-row>
+      <v-form ref="form" v-model="valid" lazy-validation>
+        <v-row class="pl-3">
+          <h4>이메일을 통해 본인임을 인증합니다.</h4>
+        </v-row>
+        <v-row>
+          <v-col class="py-0 pr-3" cols="9">
+            <v-text-field
+              v-model="user.email"
+              :rules="emailRules"
+              label="사용가능한 이메일을 입력해주세요."
+            ></v-text-field>
+          </v-col>
+          <v-col class="px-0" cols="3">
+            <v-btn class="px-0" @click="sendemail">인증하기</v-btn>
+          </v-col>
+        </v-row>
+        <v-row v-if="issendemail">
+          <v-col class="py-0 pr-3" cols="8">
+            <v-text-field
+              v-model="aouthcode"
+              required
+              label="인증번호 입력"
+            ></v-text-field>
+          </v-col>
+          <v-col class="px-0" cols="3">
+            <v-btn class="px-0" @click="codecheck">인증번호 확인</v-btn>
+          </v-col>
+        </v-row>
+        <!-- email 인증 추가하기 -->
+        <v-row>
+          <v-col class="py-0" cols="8">
+            <!-- id input -->
+            <v-text-field
+              v-model="user.userid"
+              label="아이디를 입력해주세요."
+              :rules="[rules.required]"
+              @change="userIdChk = false"
+            ></v-text-field>
+          </v-col>
+          <v-col cols="4">
+            <v-btn class="px-0" @click="idcheck">중복 검사</v-btn>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col class="py-0">
+            <!-- password input -->
+            <v-text-field
+              v-model="user.userpwd"
+              :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+              :rules="[rules.required, rules.min]"
+              :type="show ? 'text' : 'password'"
+              label="비밀번호를 설정해주세요."
+              hint="8자 이상 입력하세요"
+              @click:append="show = !show"
+              counter
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col class="py-0">
+            <!-- password input again -->
+            <v-text-field
+              v-model="chkPassword"
+              :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+              :rules="[rules.required, rules.min]"
+              :type="show ? 'text' : 'password'"
+              label="비밀번호를 다시 한 번 입력해주세요."
+              hint="8자 이상 입력하세요"
+              @click:append="show = !show"
+              counter
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row justify="center">
+          <h5 v-if="!sameChk(chkPassword)" class="pink--text">
+            비밀번호가 같지 않습니다. 다시 한 번 확인해주세요.
+          </h5>
+        </v-row>
+        <v-row>
+          <v-col class="py-0" cols="8">
+            <v-form ref="nicknameForm" lazy-validation>
+              <!-- nickname input -->
+              <v-text-field
+                v-model="user.usernickname"
+                :counter="10"
+                label="닉네임을 입력해주세요."
+                :rules="nameRules"
+                @change="userNickChk = false"
+              ></v-text-field
+            ></v-form>
+          </v-col>
+          <v-col cols="4">
+            <v-btn class="px-0" @click="nickcheck" @keyup.enter="nickcheck"
+              >중복 검사</v-btn
+            >
+          </v-col>
+        </v-row>
+        <v-row justify="center">
+          <h5>
+            옥순이 캐릭터를 골라주세요. 차후 포인트 차감 후 변경 가능합니다.
+          </h5>
+        </v-row>
+        <v-row justify="center">
+          <v-img
+            max-width="30%"
+            :src="
+              require(`../assets/img/Ocsoon/Character/${this.user.avatar}.png`)
+            "
+          >
+            <v-img src="../assets/img/Ocsoon/Face/0.png"></v-img>
+          </v-img>
+        </v-row>
+        <v-row justify="center" class="py-0">
+          <v-btn max-height="30" color="#fa183e" @click="changeColor(0)" />
+          <v-btn max-height="30" color="#ffbbed" @click="changeColor(1)" />
+          <v-btn max-height="30" color="#ffec00" @click="changeColor(2)" />
+          <v-btn max-height="30" color="#b9ffb2" @click="changeColor(3)" />
+        </v-row>
+        <v-row justify="center" class="py-0">
+          <v-btn max-height="30" color="#a5bbff" @click="changeColor(7)" />
+          <v-btn max-height="30" color="#003fff" @click="changeColor(4)" />
+          <v-btn max-height="30" color="#d2a1ff" @click="changeColor(5)" />
+          <v-btn max-height="30" color="#8500ff" @click="changeColor(6)" />
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-btn outlined @click="reset" color="red">Reset</v-btn>
+          </v-col>
+          <v-col></v-col>
+          <v-col>
+            <v-btn color="#ffadad" @click="register()">회원가입하기</v-btn>
+          </v-col>
+        </v-row>
+      </v-form>
+    </v-container>
+  </div>
 </template>
 
 <script>
 import axios from "axios";
 export default {
-  data:()=>({
-    user:{
+  data: () => ({
+    user: {
       userid: null,
       userpwd: null,
       usernickname: null,
@@ -165,8 +175,7 @@ export default {
     userIdChk: true,
     userNickChk: true,
     codeChk: false,
-    valid : false,
-    
+    valid: false,
 
     avatars: [
       { num: 0 },
@@ -183,21 +192,21 @@ export default {
       (v) => !!v || "Name is required",
       (v) => (v && v.length <= 10) || "Name must be less than 10 characters",
       (v) => (v && v.length >= 4) || "Name must be at least 4 characters",
-     
     ],
     isError: false,
     errorMsg: "",
     emailRules: [
       (v) => !!v || "E-mail is required",
-      (v) => { 
-        if(v){
-        const replaceV = v.replace(/(\s*)/g, '')
-        const pattern = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/
-        return pattern.test(replaceV) || 'E-mail must be valid'}
-        else{
-          return !!v || "E-mail is required"
+      (v) => {
+        if (v) {
+          const replaceV = v.replace(/(\s*)/g, "");
+          const pattern =
+            /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/;
+          return pattern.test(replaceV) || "E-mail must be valid";
+        } else {
+          return !!v || "E-mail is required";
         }
-        },
+      },
     ],
     show: false,
     chkPassword: "",
@@ -206,14 +215,14 @@ export default {
       min: (v) => (v && v.length >= 8) || "Min 8 characters",
     },
   }),
-  methods:{
+  methods: {
     changeColor(value) {
-      this.user.avatar= value;
+      this.user.avatar = value;
     },
-    goback(){
+    goback() {
       this.$router.go(-1);
     },
-    login(){
+    login() {
       this.$router.push({ name: "Login" });
     },
     idcheck() {
@@ -241,26 +250,26 @@ export default {
     },
     nickcheck() {
       const validate = this.$refs.nicknameForm.validate();
-      if(validate){
-      axios
-        .get(`api/register/check/nickname/${this.user.usernickname}`)
-        .then(({ data }) => {
-          let msg = "중복된 닉네임입니다. 다시 입력해주세요";
-          if (data === false) {
-            msg = "사용가능한 닉네임입니다.";
-            this.userNickChk = true;
-            alert(msg);
-          } else {
-            this.userNickChk = false;
-            alert(msg);
-          }
-        })
-        .catch((error) => {
-          console.log(error);
-          alert("닉네임중복체크에 실패했습니다..");
-        });
-         }else{
-        alert("닉네임이 유효하지 않습니다.")
+      if (validate) {
+        axios
+          .get(`api/register/check/nickname/${this.user.usernickname}`)
+          .then(({ data }) => {
+            let msg = "중복된 닉네임입니다. 다시 입력해주세요";
+            if (data === false) {
+              msg = "사용가능한 닉네임입니다.";
+              this.userNickChk = true;
+              alert(msg);
+            } else {
+              this.userNickChk = false;
+              alert(msg);
+            }
+          })
+          .catch((error) => {
+            console.log(error);
+            alert("닉네임중복체크에 실패했습니다..");
+          });
+      } else {
+        alert("닉네임이 유효하지 않습니다.");
       }
     },
     codecheck() {
@@ -297,9 +306,12 @@ export default {
         .then(({ data }) => {
           console.log(data);
           let msg = "email 전송에 실패했습니다..";
-          if (data === true) {
+          if (data === "전송성공") {
             msg = "인증번호가 전송되었습니다.";
             this.issendemail = true;
+            alert(msg);
+          } else if (data === "이메일중복") {
+            msg = "이미 가입된 이메일입니다.";
             alert(msg);
           } else {
             alert(msg);
@@ -326,18 +338,17 @@ export default {
       }
     },
     register() {
-
       if (
         !this.user.email ||
         !this.user.usernickname ||
         !this.user.userpwd ||
-        this.user.avatar===null ||
+        this.user.avatar === null ||
         !this.codeChk ||
         !this.userIdChk ||
         !this.userNickChk
       ) {
         this.isError = true;
-        
+
         this.errorMsg = "이메일과 닉네임과 비밀번호를 모두 입력해주세요.";
         return;
       }
@@ -360,9 +371,7 @@ export default {
           console.log(error);
           alert("등록 실패입니다.");
         })
-        .finally(() => {
-
-        });
+        .finally(() => {});
     },
     validate() {
       this.$refs.form.validate();
@@ -378,10 +387,8 @@ export default {
       this.user.avatar = i;
       console.log(this.user.avatar);
     },
-  }
-}
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>

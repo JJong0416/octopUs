6c9f4a48a5f025170fe43b6a14fbd62c5f759c2b
DESKTOP-KVCQHCD\SSAFY <lapaho8645@naver.com>
2022-08-17 21:50:19 +0900

    aboutus

--- frontend/src/views/Information/AboutusView.vue
+++ frontend/src/views/Information/AboutusView.vue
@@ -1,78 +1,34 @@
 <template>
-<div>
-  <!-- 뒤로 가기 버튼 -->
+<div class="wrap">
+    <v-container>
+      <v-row>
+        <v-col>
+          <!-- 뒤로 가기 버튼 -->
           <v-btn @click="goback" icon>
             <v-icon> mdi-arrow-left</v-icon>
           </v-btn>
-  <v-row align="center">
-    <v-item-group
-      v-model="window"
-      class="shrink mr-6"
-      mandatory
-      tag="v-flex"
-    >
-      <v-item
-        v-for="n in length"
-        :key="n"
-        v-slot="{ active, toggle }"
-      >
-        <div>
-          <v-btn
-            :input-value="active"
-            icon
-            @click="toggle"
-          >
-            <v-icon>mdi-record</v-icon>
-          </v-btn>
-        </div>
-      </v-item>
-    </v-item-group>
-
-    <v-col>
-      <v-window
-        v-model="window"
-        class="elevation-1"
-        vertical
-      >
-        <v-window-item
-          v-for="n in length"
-          :key="n"
-        >
-          <v-card flat>
-            <v-card-text>
-              <v-row
-                class="mb-4"
-                align="center"
-              >
-                <v-avatar
-                  color="grey"
-                  class="mr-4"
-                ></v-avatar>
-                <strong class="text-h6">Title {{ n }}</strong>
-                <v-spacer></v-spacer>
-                <v-btn icon>
-                  <v-icon>mdi-account</v-icon>
-                </v-btn>
-              </v-row>
-
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </p>
-
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </p>
-            </v-card-text>
-          </v-card>
-        </v-window-item>
-      </v-window>
-    </v-col>
-  </v-row>
-  </div>
+        </v-col>
+      </v-row>
+  <v-img src="../../assets/img/aboutus/1.png"></v-img>
+  <br><br>
+  <v-img src="../../assets/img/aboutus/2.png"></v-img>
+  <br><br>
+  <v-img src="../../assets/img/aboutus/3.png"></v-img>
+  <br><br>
+   <v-img src="../../assets/img/aboutus/4.png"></v-img>
+   <br><br>
+    <v-img src="../../assets/img/aboutus/5.png"></v-img>
+    <br><br>
+     <v-img src="../../assets/img/aboutus/6.png"></v-img>
+     <br><br>
+    </v-container>
+</div>
 </template>
 
 <script>
+
   export default {
+     
     data: () => ({
       length: 3,
       window: 0,
@@ -85,4 +41,6 @@
   }
 </script>
 
-<style></style>
+<style>.wrap {
+  background-color: #f7f5fc;
+}</style>
